4de1090f43c85af1f7d5d8116344364b4481358d
amozon-clone <sudha0987yad@gmail.com>
2025-12-14 14:08:39 +0530

    projects updates

--- script.js
+++ script.js
@@ -1,48 +1,36 @@
- 
 var typed = new Typed(".texts", {
-    strings: ["Frontend Developer", "Ui-Ux Designer", "Web Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
+  strings: ["Frontend Developer", "Ui-Ux Designer", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
 
-var tablinks= document.getElementsByClassName('tab-links');
-var tabcontents= document.getElementsByClassName('tab-contents');
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-        
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab")
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
 
-
-const mobileMenuBtn=document.querySelector('.mobile-menu');
-const navLinks=document.querySelector('.nav-links');
-mobileMenuBtn.addEventListener('click',()=>{
-    navLinks.classList.toggle('active');
-});
- 
-
-document.querySelector('a[href^="#"]').array.forEach(anchor => {
-    anchor.addEventListener('click', function(e){
-        e.preventDefault();
-        const target= document.querySelector(this.getAttribute('href'));
-        if(target){
-            target.scrollIntoView({behavior:'smooth'});
-
-        }
-
-    })
+const mobileMenuBtn = document.querySelector(".mobile-menu");
+const navLinks = document.querySelector(".nav-links");
+mobileMenuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
 
-
- 
-
- 
+document.querySelector('a[href^="#"]').array.forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
